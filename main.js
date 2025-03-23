b45cc3d21f384e4cb62766558516ded52921a990
Ayaz <aa1552582@gmail.com>
2025-03-24 01:42:41 +0530

    some changes in files

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set("view engine", 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 app.get('/', (req, res) => {
     res.render('index')
