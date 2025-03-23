2d49a729b1c8cc1d959caec0f423ffe572b12f7a
Ayaz <aa1552582@gmail.com>
2025-03-24 02:24:08 +0530

    some changes

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
 
 app.set("view engine", 'ejs')
 app.set('views', path.join(__dirname, 'views'));
@@ -11,11 +14,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('index')
 })
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+
 
 app.post('/login-form', (req, res) => {
-    console.log(req.body)
+    console.log("Received Data:", req.body);
     res.send("data recived")
 })
 app.post('/register-Form', (req, res) => {
