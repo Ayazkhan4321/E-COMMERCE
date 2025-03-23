86cd24ec380d3792f1be70f3bf81c5c8f3f73ee2
Ayaz <aa1552582@gmail.com>
2025-03-24 01:44:49 +0530

    some changes in files

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('index')
 })
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
